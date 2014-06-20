9edd09fc670bd543913e99c3bbc2e8cff7e9dfd2
josiastech <josiastech@gmail.com>
2014-06-20 15:21:23 -0700

    Update the project using Angular and yeoman scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sfotipyApp
+ * @description
+ * # sfotipyApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sfotipyApp', [
+    'ngAnimate',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sfotipyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sfotipyApp
+ */
+angular.module('sfotipyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
