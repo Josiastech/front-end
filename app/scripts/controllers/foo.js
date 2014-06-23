a2fc3892d0c97369586eac30543f8bb66aea1e47
josiastech <josiastech@gmail.com>
2014-06-23 06:45:59 -0700

    Update local demo

---
+++ app/scripts/controllers/foo.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sfotipyApp.controller:FooCtrl
+ * @description
+ * # FooCtrl
+ * Controller of the sfotipyApp
+ */
+angular.module('sfotipyApp')
+  .controller('FooCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
